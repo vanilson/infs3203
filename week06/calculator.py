67dfe52eec0375dc2ed2642f277f95d2a97e42ce
Mohammed AlMawlawi <145222313+MohammedAlMawlawi@users.noreply.github.com>
2024-03-15 20:33:12 +0300

    bugs fixes and code enhancement for math functions only\nMain functions will be different branch

--- week06/calculator.py
+++ week06/calculator.py
@@ -1,26 +1,96 @@
 class Calculator:
-    def add(self, x, y):
+    '''This function returns the float of adding two float operands
+        :param x: x is the first float or an int number
+        :param y: y is the second float or an int number
+        
+        #Bug(s) fixed
+         -Nothing
+
+        #Features added
+         -Just the doc-string via comments
+    '''
+    def add(self, x : float, y: float) -> float:
         return x + y
 
-    def subtract(self, x, y):
-        return x + y
+    '''This function returns the float of subtracting two float operands
+        :param x: x is the first float or an int number
+        :param y: y is the second float or an int number
 
-    def multiply(self, x, y):
+        #Bug(s) fixed
+         -changed the operator from - to +
+        
+        #Features added
+         -Just the doc-string via comments
+    '''
+    def subtract(self, x : float, y : float) -> float:
+        return x - y
+
+
+    '''This function returns the float of multiplying two float operands
+        :param x: x is the first float or an int number
+        :param y: y is the second float or an int number
+
+        #Bug(s) fixed
+         -Nothing found
+
+        #Features added
+         -Just the doc-string via comments
+    '''
+    def multiply(self, x : float, y : float) -> float:
         return x * y
+    
+    '''This function returns the float of dividing two float operands
+        :param x: x is the first float or an int number
+        :param y: y is the second float or an int number
 
-    def divide(self, x, y):
+        #Bug(s) fixed
+         -Nothing found
+
+        #Features added
+         -Just the doc-string via comments
+    '''
+    def divide(self, x : float, y : float) -> float:
         if y == 0:
             return "Cannot divide by zero"
         return x / y
 
-    def modulo(self, x, y):
+    '''This function returns the int of modulo between two float operands
+        :param x: x is the first float or an int number
+        :param y: y is the second float or an int number
+
+        #Bug(s) fixed
+         -Nothing found
+
+        #Features added
+         -Just the doc-string via comments
+    '''
+    def modulo(self, x : float, y : float) -> int:
         return x % y
 
-    def power(self, x, y):
+    '''This function returns the float of the specific float power of a given float operand
+        :param x: {float} x is the number float or an int number
+        :param y: {float} y is the power float or an int number
+
+        #Bug(s) fixed
+         -Nothing found
+
+        #Features added
+         -Just the doc-string via comments
+    '''
+    def power(self, x, y) -> float:
         return x ^ y
 
-    def square_root(self, x):
-        return x ** (1 / 2)
+    '''This function returns the float of square root of the given float operand
+        :param x: {float} x is the float or an int number
+        
+        #Bug(s) fixed
+         -Algorithm changed from power (1/2) to .5
+
+        #Features added
+         -Just the doc-string via comments
+    '''
+    def square_root(self, x) -> float:
+        return x ** .5
 
 
 def main():
